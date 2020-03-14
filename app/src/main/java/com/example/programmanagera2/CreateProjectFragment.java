62613d77a07e12dc4d69c8dc5d2402fcb2bae2a7
aa-ronn <55965197+aa-ronn@users.noreply.github.com>
2020-03-14 13:06:28 -0400

    Updated Create Project Page

--- app/src/main/java/com/example/programmanagera2/CreateProjectFragment.java
+++ app/src/main/java/com/example/programmanagera2/CreateProjectFragment.java
@@ -4,19 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class CreateProjectFragment extends Fragment {
-    private int year, month, day;
-    private Calendar calendar;
     private EditText dateStart;
     private EditText dateEnd;
+    private EditText personFirstName;
+    private EditText personSecondName;
+    private SeekBar personSkillLevel;
+    private ListView personListview;
+    private ArrayList<String> personList;
+    private  ArrayAdapter<String> arrayAdapter;
+    private String listString;
 
     @Override
     public View onCreateView(
@@ -24,7 +34,8 @@ public class CreateProjectFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_project, container, false);
+        View v=inflater.inflate(R.layout.fragment_create_project, container, false);
+        return v;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -32,15 +43,31 @@ public class CreateProjectFragment extends Fragment {
         //get date text boxes
         dateStart = (EditText) view.findViewById(R.id.editText_Start_Date);
         dateEnd = (EditText) view.findViewById(R.id.editText_End_Date);
+        personFirstName =(EditText)view.findViewById(R.id.editText_person_first_name);
+        personSecondName =(EditText)view.findViewById(R.id.editText_person_last_name);
+        personSkillLevel=(SeekBar)view.findViewById(R.id.seekBar_skill_level);
+        Button btnPerson = (Button) view.findViewById(R.id.button_add_person);
+        personListview =view.findViewById(R.id.listView_people);
+        personList =new ArrayList<>();
+        arrayAdapter =new ArrayAdapter<String>(view.getContext(),android.R.layout.simple_list_item_1, personList);
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month +1, day);
+        btnPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listString= personSkillLevel.getProgress()+" "+personFirstName.getText().toString()+" "+ personSecondName.getText().toString();
+                personList.add(listString);
+                personListview.setAdapter(arrayAdapter);
+                personFirstName.setText("");
+                personSecondName.setText("");
+                personSkillLevel.setProgress(0);
+            }
+        });
 
-        calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        showDate(year, month+1, day);
-
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_done).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(CreateProjectFragment.this)
@@ -57,3 +84,6 @@ public class CreateProjectFragment extends Fragment {
                 .append(month).append("/").append(year));
     }
 }
+
+//call insertProject()
+//just pass 2 variables
