5a94e55d639197f72ee0b3e277886ba5bca02075
Manthan Rami <55422514+ManthanRami@users.noreply.github.com>
2020-03-14 12:27:28 -0400

    ListView Fixed

--- app/src/main/java/com/example/programmanagera2/CreateProjectFragment.java
+++ app/src/main/java/com/example/programmanagera2/CreateProjectFragment.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SeekBar;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,11 +17,17 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class CreateProjectFragment extends Fragment {
+    private int year, month, day;
+    private Calendar calendar;
     private EditText dateStart;
     private EditText dateEnd;
-    ListView listView;
-    ArrayList<PersonInList> arrayOfPeople = new ArrayList<PersonInList>();
-    private static PersonListAdapter adapter;
+    private Button btnUser;
+    private EditText userName;
+    private EditText userEmail;
+    private ListView userListview;
+    private ArrayList<String> userList;
+    private  ArrayAdapter<String> arrayAdapter;
+    private String listString;
 
     @Override
     public View onCreateView(
@@ -29,68 +35,38 @@ public class CreateProjectFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_project, container, false);
+        View v=inflater.inflate(R.layout.fragment_create_project, container, false);
+        return v;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        //make an array adapter to link the list to the data array
-        adapter = new PersonListAdapter(arrayOfPeople, getActivity());
-        //attach the adapter to the list
-        listView = (ListView) view.findViewById(R.id.listView_people);
-        listView.setAdapter(adapter);
-
         //get date text boxes
         dateStart = (EditText) view.findViewById(R.id.editText_Start_Date);
         dateEnd = (EditText) view.findViewById(R.id.editText_End_Date);
-
-        //get date values
-        Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-        showDate(year, month +1, day);
-
-        final String[] fullName = {"", ""};
-
-        //set up listener for add person button
-        view.findViewById(R.id.button_add_person).setOnClickListener(new View.OnClickListener() {
+        userName=(EditText)view.findViewById(R.id.editText_Person_Name);
+        userEmail=(EditText)view.findViewById(R.id.editText_Person_Email);
+        btnUser=(Button)view.findViewById(R.id.button_Add_User);
+        userListview=view.findViewById(R.id.listView_Users);
+        userList=new ArrayList<>();
+        arrayAdapter =new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,userList);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
+        btnUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText tvFirstName = getView().findViewById(R.id.editText_person_first_name);
-                EditText tvLastName = getView().findViewById(R.id.editText_person_last_name);
-                SeekBar sbSkillLevel = getView().findViewById(R.id.seekBar_skill_level);
-                fullName[0] = tvFirstName.getText().toString();
-                fullName[1] = tvLastName.getText().toString();
-                int skillLevel = sbSkillLevel.getProgress();
-                boolean good = true;
-
-                //check is first name is empty
-                if(fullName[0].trim().isEmpty()){
-                    Toast.makeText(getView().getContext(),"Provide a first name", Toast.LENGTH_SHORT).show();
-                    good = false;
-                }
-                //check if last name is empty
-                if(fullName[1].trim().isEmpty()){
-                    Toast.makeText(getView().getContext(),"Provide a last name", Toast.LENGTH_SHORT).show();
-                    good = false;
-                }
-                //if all conditions met add to list
-                if(good){
-                    PersonInList newPerson = new PersonInList(fullName[0], fullName[1], skillLevel);
-                    adapter.add(newPerson);
-                } else {
-                    good = true;
-                    tvFirstName.setText("");
-                    tvLastName.setText("");
-                    sbSkillLevel.setProgress(0);
-                }
+                listString=userName.getText().toString()+" : "+userEmail.getText().toString();
+                userList.add(listString);
+                userListview.setAdapter(arrayAdapter);
+                userName.setText("");
+                userEmail.setText("");
             }
         });
 
-        //set up listener for done button
-        view.findViewById(R.id.button_done).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(CreateProjectFragment.this)
